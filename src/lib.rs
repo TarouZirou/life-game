f87e95885ab032507a43de982ec63ca7687d4d6e
TarouZirou <zinnnnnnnnnnnn8@gmail.com>
2025-04-20 12:19:54 +0900

    this is life game

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+	Dead = 0,
+	Alive = 1,
+}
+
+/*
+最終的に、
+[[Cell; width]; height] -> [Cell; width * height]
+*/
+#[wasm_bindgen]
+pub struct Universe {
+	width: u32,
+	height: u32,
+	cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+	fn get_index(&self, row: u32, col: u32) -> usize {
+		(row * self.width + col) as usize
+	}
+
+	//あるセルの隣のセルの数を数える
+	fn live_neighbor_count(&self, row: u32, col: u32) -> u8 {
+		let mut count = 0;
+
+		//-1を配列の中に入れ込むことはできないので、
+		//(x + (a - 1)) % aとすることによって、
+		//うまいことx-1を表現することができる
+		for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+			for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+				if delta_row == 0 && delta_col == 0 {
+					continue;
+				}
+
+				let neighbor_row = (row + delta_row) % self.height;
+				let neighbor_col = (col + delta_col) % self.width;
+				let idx = self.get_index(neighbor_row, neighbor_col);
+				count += self.cells[idx] as u8;
+			}
+		}
+		count
+	}
+
+	//時間を一つ経過させる
+	pub fn tick(&mut self) {
+		let mut next = self.cells.clone();
+
+		for row in 0..self.height {
+			for col in 0..self.width {
+				let idx = self.get_index(row, col);
+				let cell = self.cells[idx];
+				let live_neighbors = self.live_neighbor_count(row, col);
+
+				let next_cell = match (cell, live_neighbors) {
+					(Cell::Alive, x) if x < 2 => Cell::Dead,
+
+					(Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+
+					(Cell::Alive, x) if x > 3 => Cell::Dead,
+
+					(Cell::Dead, 3) => Cell::Alive,
+
+					(otherwise, _) => otherwise,
+				};
+
+				next[idx] = next_cell;
+			}
+		}
+
+		self.cells = next;
+	}
+}
